754db42cdf8cba966cb480c009dfc22188260a5f
Rashmi-Singh11 <rashmisingh110303@gmail.com>
2024-08-31 00:21:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve uploaded files
+mongoose.connect('mongodb+srv://kalpesh_158:90pvNz4OSGVkgABP@cluster0.ksqwf0d.mongodb.net/auth-db');
 
 // Routes
 const signupRoute = require('./routes/signup');
